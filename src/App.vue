2ae283e3ebe739059b9d89cae677c2ffae859535
yu-linfeng <hellobug@outlook.com>
2018-12-29 23:17:29 +0800

    配置代理实现跨域ajax请求

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
 
 <script>
   import FooterGuide from './components/FooterGuide/FooterGuide'
+  import {reqFoodTypes} from './api'
+
   export default {
+
+    async mounted() {
+      const result = await reqFoodTypes();
+      console.info(result)
+    },
     components: {
       FooterGuide
     }
---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import ajax from './ajax'
+
+const BASE_URL = '/api'
+
+/**
+ * 根据经纬度获取位置详情
+ * @param geohash
+ */
+export const reqAddress = (geohash) => ajax(`${BASE_URL}/position/${geohash}`)
+
+/**
+ * 获取食品分类列表
+ */
+export const reqFoodTypes = () => ajax(`${BASE_URL}/index_category`)
+
+/**
+ * 根据经纬度获取商铺列表
+ * @param longitude 经度
+ * @param latitude 纬度
+ */
+export const reqShops = (longitude, latitude) => ajax(`${BASE_URL}/shops`, {longitude}, {latitude})
+
+
+
+
