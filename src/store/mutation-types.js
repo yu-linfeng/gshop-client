cc34a06b43a927a7412655ff8de28c20effb328d
yu-linfeng <hellobug@outlook.com>
2018-12-29 23:31:50 +0800

    创建Vuex的整体结构

---
+++ src/store/mutation-types.js
@@ -0,0 +1,20 @@
+/**
+ * 包含n个mutation的type名称常量
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import state from './state'
+import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
+
+
